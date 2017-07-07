0e29d7c45b90f3e83591d4f77b1fe71a9a171bc2
Jonathon Wallen <jonathon@monkii.com>
2017-07-07 13:52:24 +1000

    updated view to match current state in monkii site
    (cherry picked from commit d00ddb7)

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -6,7 +6,7 @@
  * @copyright 2008 - present, Monkii Digital Agency (http://monkii.com.au)
  */
 ?>
-@extends('vendor/laravel-administrator.layout')
+@extends('vendor.laravel-administrator.layout')
 
 @section('title', 'Pages')
 
@@ -15,38 +15,33 @@
     <h1>Create page</h1>
 
     {!! Form::open(['route' => 'laravel-administrator-pages-post']) !!}
-    <div class="panel  panel__full">
-        <div class="panel__inner">
 
-            <div class="panel__row">
-                <div class="panel__full">
-                    <h4>Enter a title</h4>
-                </div>
-                <div class="panel__full">
-                    <fieldset class="{{ $errors->has('title') ? 'error' : '' }}">
-                        {!! Form::text('title') !!}
-                        <div class="form__error">{{ $errors->first('title') }}</div>
-                    </fieldset>
-                </div>
+    <div class="row">
+        <div class="col-md-6">
+
+            <!-- Title -->
+            <div class="form-group">
+                <fieldset class="{{ $errors->has('title') ? 'error' : '' }}">
+                    <label for="create-page-title">Enter a title</label>
+                    {!! Form::text('title', '', array('id' => 'create-page-title', 'class' => 'form-control')) !!}
+                    <div class="form__error">{{ $errors->first('title') }}</div>
+                </fieldset>
             </div>
 
-            <div class="panel__row">
-                <div class="panel__full">
-                    <h4>Select a page type</h4>
-                </div>
 
-                <div class="panel__full">
-                    <fieldset class="{{ $errors->has('page_type') ? 'error' : '' }}">
-                        {!! Form::select('page_type', ['' => 'Please select a page type'] + $types) !!}
-                        <div class="form__error">{{ $errors->first('page_type') }}</div>
-                    </fieldset>
-                </div>
+            <!-- Page type -->
+            <div class="form-group">
+                <fieldset class="{{ $errors->has('page_type') ? 'error' : '' }}">
+                    <label for="create-page-type">Select a page type</label>
+                    {!! Form::select('page_type', ['' => 'Please select a page type'] + $types, null, array('id' => 'create-page-type', 'class' => 'form-control')) !!}
+                    <div class="form__error">{{ $errors->first('page_type') }}</div>
+                </fieldset>
             </div>
 
+
+            <input name="submit" type="submit" value="Save and edit" class="btn  btn-primary">
         </div>
     </div>
-
-    {!! Form::submit('Save and edit', ['name' => 'submit']) !!}
 
     {!! Form::close() !!}
 

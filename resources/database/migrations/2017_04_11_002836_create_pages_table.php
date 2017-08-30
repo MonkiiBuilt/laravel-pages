b88f108c325d1e965355c3586ae12df53ef6e5f4
Jonathon Wallen <jonathon@monkii.com>
2017-08-30 15:35:31 +1000

    Re installed slug column, updated promoted delta column to have a default value

--- resources/database/migrations/2017_04_11_002836_create_pages_table.php
+++ resources/database/migrations/2017_04_11_002836_create_pages_table.php
@@ -16,13 +16,13 @@ class CreatePagesTable extends Migration
         Schema::create('pages', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
-
             $table->string('title', 255);
+            $table->string('slug', 255);
             $table->string('page_type', 255);
             $table->boolean('published')->default(0);
             $table->smallInteger('promoted')->default(0);
             $table->smallInteger('delta')->default(0);
-            $table->smallInteger('promoted_delta');
+            $table->smallInteger('promoted_delta')->default(0);
             $table->integer('created_by')->unsigned();
             $table->integer('updated_by')->unsigned();
 
